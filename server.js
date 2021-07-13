f15fa7338d1fbbfb6a9f73336efc5963c2084012
Kelloo <ahmadelkilany23@gmail.com>
2021-07-13 17:50:39 +0200

    installed dependencies

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-
+// Connect to database
+connectDB();
 //app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
