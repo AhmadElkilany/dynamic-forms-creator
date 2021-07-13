86c66080b81e64e61779316810d982b5a4c3ef47
Kelloo <ahmadelkilany23@gmail.com>
2021-07-13 22:09:28 +0200

    Created Form Model - Image

---
+++ models/Form.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const FormSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  fields: {
+    type: [String],
+    required: true,
+    default: ['Full Name', 'Phone Number'],
+  },
+});
+
+module.exports = Form = mongoose.model('form', FormSchema);
