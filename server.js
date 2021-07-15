138bf939819a7995c7c102119db44cf410a0d1c2
Kelloo <ahmadelkilany23@gmail.com>
2021-07-15 17:08:22 +0200

    Added image handling to APIs

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const multer = require('multer');
+var fs = require('fs');
+var path = require('path');
 const connectDB = require('./config/db');
 const Form = require('./models/Form');
 const { check, validationResult } = require('express-validator');
@@ -7,6 +10,30 @@ const app = express();
 // Connect to database
 connectDB();
 app.use(express.json({ extended: false }));
+
+const fileFilter = (req, file, cb) => {
+  //only accept images
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png')
+    cb(null, true);
+  else cb(null, false);
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({
+  storage: storage,
+  limits: {
+    //only accept files <= 4MB
+    fileSize: 1024 * 1024 * 4,
+  },
+  fileFilter: fileFilter,
+});
 
 ///////////// Create Required APIs ///////////////////////////
 
@@ -21,22 +48,54 @@ app.get('/', async (req, res) => {
   }
 });
 
+// get a form
+app.get('/:id', async (req, res) => {
+  try {
+    const form = await Form.findById(req.params.id);
+    if (!form) return res.status(404).json({ msg: 'Form Not Found' });
+    res.json(form);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
 // create a form
 app.post(
   '/',
+  upload.single('image'),
   [
     check('title', 'Title is required').not().isEmpty(),
     check('description', 'Description is required').not().isEmpty(),
+    check('fields', 'Fields are required').not().isEmpty(),
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    const newFields = req.body.fields;
+    if (
+      !(newFields.includes('Full Name') && newFields.includes('Phone Number'))
+    ) {
+      return res
+        .status(400)
+        .json({ errors: 'Full name and phone number are required' });
+    }
+    if (!req.file) {
+      return res.status(400).json({ errors: 'Image is required' });
+    }
     try {
+      console.log(req.file);
       const newForm = new Form({
         title: req.body.title,
         description: req.body.description,
+        image: {
+          data: fs.readFileSync(
+            path.join(__dirname + '/uploads/' + req.file.filename)
+          ),
+          contentType: req.file.mimetype,
+        },
         fields: req.body.fields,
       });
 
@@ -61,6 +120,40 @@ app.delete('/:id', async (req, res) => {
     if (error.kind === 'ObjectId')
       return res.status(404).json({ msg: 'Form not found' });
     res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
+// update a form
+app.put('/:id', upload.single('image'), async (req, res) => {
+  const image = req.file;
+  const fields = req.body.fields;
+  const title = req.body.title;
+  const description = req.body.description;
+  const updateFields = {};
+  if (image) {
+    updateFields.image = {
+      data: fs.readFileSync(
+        path.join(__dirname + '/uploads/' + req.file.filename)
+      ),
+      contentType: req.file.mimetype,
+    };
+  }
+  if (fields) updateFields.fields = fields;
+  if (title) updateFields.title = title;
+  if (description) updateFields.description = description;
+
+  try {
+    let form = await Form.findOne({ _id: req.params.id });
+    if (form) {
+      form = await Form.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: updateFields }
+      );
+      return res.json(form);
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
   }
 });
 
